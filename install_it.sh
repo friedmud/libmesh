4945d56ffc1780072263e17693d1d2a8fb911f7e
Derek Gaston <derek.gaston@inl.gov>
2013-02-09 23:50:33 -0700

    gastdr install scripts

---
+++ install_it.sh
@@ -0,0 +1,10 @@
+mkdir lib
+
+for i in $METHODS
+do
+  ${LIBMESH_DIR}/libtool --mode=install install -c libmesh_$i.la ${LIBMESH_DIR}/lib
+done
+
+${LIBMESH_DIR}/libtool --mode=install install -c ${LIBMESH_DIR}/contrib/netcdf/4.2.1.1/liblib/libnetcdf.la ${LIBMESH_DIR}/lib
+
+ln -s ${LIBMESH_DIR}/include/libmesh/netcdf.h ${LIBMESH_DIR}/include/netcdf.h
